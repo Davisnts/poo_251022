08471d6229859a8873a30e4c1701b472d6efb63e
Davisnts <davitpontas@gmail.com>
2022-10-25 21:26:56 -0300

    V0.1

--- heranca.js
+++ heranca.js
@@ -1,35 +1,25 @@
-//1-Criar classe Pai/SuperClasse com construtor contendo 1 atributo chamado: rodas 
-//2- Dentro da SUPERCLASSE cria um metodo chamdo acelerar()
+//Criar classe Pai/SuperClasse com construtor contendo 1 atributo chamado: rodas 
+//Dentro da SUPERCLASSE cria um metodo chamdo acelerar()
 
-class Veiculo{ 
-    constructor (rodas){
-        this.rodas = rodas;
+class Funcionario{ 
+    constructor (nome,rg,salario){
+        this.nome = nome;
+        this.rg = rg;
+        this.salario = salario;
 }
     //metodos
-    acelerar(){
-        console.log('KATIAUUUUUUUUUUU');
+    aumentar(valor){
+        console.log(`AEEE SEU SALARIO AUMENTOU EM ${valor}`);
+    }
+    pagar(nome){
+        console.log(`O Funcionario ${nome} foi pago`)
     }
 }
-//4-Criar a Subclasse Moto Da SuperClasse com um metodo empinar()
-class Moto extends Veiculo{
-    empinar(){
-        console.log('Você empinou e caiu...')
+//Criar a Subclasse Chefe Da SuperClasse com um metodo pagar()
+class Chefe extends Funcionario{
+    constructor(gastro_extra,adicional_chefia){
+        this.gastro_extra = gastro_extra;
+        this.adicional_chefia = adicional_chefia;
     }
-    
+    pagar()
 }
-//criar subclasse moto2 herdando 
-class Moto2 extends Veiculo{ 
-    acelerar(){
-        super.acelerar(){
-            console.log('VRUUUUUUUUUUUUUUUUUM')
-        }
-    }
-}
-//3-Instanciar classe veiculo - new
-const voyage = new Veiculo(4);
-//instanciar objeto fuscar para acessar o metodo empinar
-const voyage1 = new Moto(4);
-//Chamando metodo empinar da moto
-voyage1.empinar()
-//chamar o metodo acelerar usando a classe moto?
-voyage1.acelerar()
